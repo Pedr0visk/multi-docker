11c29dd1027b1f4a56c5632b91d4e9299c7211a4
Pedro Henrique Souza Santos <pedro357bm@gmail.com>
2020-12-30 22:39:08 -0300

    project copied from udemy

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Fib extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      seenIndexes: [],
-      values: {},
-      index: '',
-    };
-  }
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: '',
+  };
 
   componentDidMount() {
     this.fetchValues();
